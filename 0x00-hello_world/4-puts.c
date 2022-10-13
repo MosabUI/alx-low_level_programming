a4553af12df94a5e99cf75b65a580ecadcbfbcf1
MosabUI <mossap100@gmail.com>
2022-10-13 07:01:26 -0700

    task 1-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+
+
 /**
  *
- * *main - Entry point
+ * * main - Printing
  *
- * *Return: Always 0 (Success)
+ * *
+ *
+ * * Return: Always 0 (Successful)
  *
  * */
-
-
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
 
 }
