a59c4253329a41585fa4f34253e2af8a0eadb54c
MosabUI <mossap100@gmail.com>
2022-10-18 08:09:06 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 
-
-
-
 /**
  *
  *  * main -Entry point of the program
@@ -13,13 +10,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
 		printf("_putchar\n");
-
-			return (0);
-
+		return (0);
 }
